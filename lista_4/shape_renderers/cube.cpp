065ad5dc4743a914ccbef466e486c15a2e491bc3
zbigniewjerzydrozd@gmail.com <zbigniewjerzydrozd@gmail.com>
2020-11-15 04:44:30 +0100

    Working mouse and keyboard

--- lista_4/shape_renderers/cube.cpp
+++ lista_4/shape_renderers/cube.cpp
@@ -1,102 +1,181 @@
-#ifndef circle_cpp_defined
-    #define circle_cpp_defined
 
-    #include <stdlib.h>
-    #include <stdio.h>
-    
-    #include <math.h>
-    
-    #include "../AGL3Window.hpp"
-    #include "../AGL3Drawable.hpp"
-    
-    #define PI 3.14159265
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <math.h>
+
+#include "../AGL3Window.hpp"
+#include "../AGL3Drawable.hpp"
+
+#define PI 3.14159265
 
 /* compiler take the wheel */
 
-// ==========================================================================
-// Drawable object with some data in buffer and vertex/fragment programs
-// ==========================================================================
-class MyCircle : public AGLDrawable {
-public:
-   MyCircle(int vertex_count) : AGLDrawable(0) {
-      setShaders();
-      setBuffers(vertex_count);
-   }
-   void setShaders() {
-      compileShaders(R"END(
 
-         #version 330 
-         #extension GL_ARB_explicit_uniform_location : require
-         #extension GL_ARB_shading_language_420pack : require
-         layout(location = 0) in vec2 pos;
-         layout(location = 0) uniform float scale;
-         layout(location = 1) uniform vec2  center;
-         out vec4 vtex;
 
-         void main(void) {
-            vec2 p = (pos * scale + center);
-            gl_Position = vec4(p, 0.0, 1.0);
-         }
+class Cube {
+    private:
+        GLuint vertexbuffer;
+        GLuint colorbuffer;
+        GLuint VertexArrayID;
+        GLuint programID;
+        GLuint MatrixID;
+        GLuint PositionID;
 
-      )END", R"END(
+        void set_buffers() {
+            bindBuffers();
+            static const GLfloat g_vertex_buffer_data[] = { 
+                -1.0f,-1.0f,-1.0f,
+                -1.0f,-1.0f, 1.0f,
+                -1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f,-1.0f,
+                -1.0f,-1.0f,-1.0f,
+                -1.0f, 1.0f,-1.0f,
+                1.0f,-1.0f, 1.0f,
+                -1.0f,-1.0f,-1.0f,
+                1.0f,-1.0f,-1.0f,
+                1.0f, 1.0f,-1.0f,
+                1.0f,-1.0f,-1.0f,
+                -1.0f,-1.0f,-1.0f,
+                -1.0f,-1.0f,-1.0f,
+                -1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f,-1.0f,
+                1.0f,-1.0f, 1.0f,
+                -1.0f,-1.0f, 1.0f,
+                -1.0f,-1.0f,-1.0f,
+                -1.0f, 1.0f, 1.0f,
+                -1.0f,-1.0f, 1.0f,
+                1.0f,-1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f,-1.0f,-1.0f,
+                1.0f, 1.0f,-1.0f,
+                1.0f,-1.0f,-1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f,-1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f,-1.0f,
+                -1.0f, 1.0f,-1.0f,
+                1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f,-1.0f,
+                -1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f, 1.0f,
+                1.0f,-1.0f, 1.0f
+            };
 
-         #version 330 
-         #extension GL_ARB_explicit_uniform_location : require
-         layout(location = 3) uniform vec3  cross_color;
-         out vec4 color;
+            // One color for each vertex. They were generated randomly.
+            static GLfloat g_color_buffer_data[] = { 
+                -1.0f,-1.0f,-1.0f,
+                -1.0f,-1.0f, 1.0f,
+                -1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f,-1.0f,
+                -1.0f,-1.0f,-1.0f,
+                -1.0f, 1.0f,-1.0f,
+                1.0f,-1.0f, 1.0f,
+                -1.0f,-1.0f,-1.0f,
+                1.0f,-1.0f,-1.0f,
+                1.0f, 1.0f,-1.0f,
+                1.0f,-1.0f,-1.0f,
+                -1.0f,-1.0f,-1.0f,
+                -1.0f,-1.0f,-1.0f,
+                -1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f,-1.0f,
+                1.0f,-1.0f, 1.0f,
+                -1.0f,-1.0f, 1.0f,
+                -1.0f,-1.0f,-1.0f,
+                -1.0f, 1.0f, 1.0f,
+                -1.0f,-1.0f, 1.0f,
+                1.0f,-1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f,-1.0f,-1.0f,
+                1.0f, 1.0f,-1.0f,
+                1.0f,-1.0f,-1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f,-1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f,-1.0f,
+                -1.0f, 1.0f,-1.0f,
+                1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f,-1.0f,
+                -1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                -1.0f, 1.0f, 1.0f,
+                1.0f,-1.0f, 1.0f
+            };
 
-         void main(void) {
-            color = vec4(cross_color,1.0);
-         } 
+        //for(int i = 0; i < 12*3*3; i++)
+        //    g_color_buffer_data[i] -= 0.5f;
+        
+        glGenBuffers(1, &vertexbuffer);
+    	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+        AGLErrors("zmienne");
 
-      )END");
-   }
-    void setBuffers(int vertex_count) {
-        bindBuffers();
-        float size_mod = 3.0;
-        __vertex_count = vertex_count;
-        GLfloat vert[vertex_count * 3][3];
-        for (int i = 0; i < vertex_count; i ++) {
-            vert[3*i][0] = cos(PI * (360.0 * float(i) / float(vertex_count)) / 180.0) * size_mod;
-            vert[3*i][1] = sin(PI * (360.0 * float(i) / float(vertex_count)) / 180.0) * size_mod;
-            vert[3*i][2] = 0.0;
+        glGenBuffers(1, &colorbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
-            vert[3*i + 1][0] = cos(PI * (360.0 * float(i + 1) / float(vertex_count)) / 180.0) * size_mod;
-            vert[3*i + 1][1] = sin(PI * (360.0 * float(i + 1) / float(vertex_count)) / 180.0) * size_mod;
-            vert[3*i + 1][2] = 0.0;
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(
+            0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+            3,                  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+        );
 
-            vert[3*i + 2][0] = 0.0;
-            vert[3*i + 2][1] = 0.0;
-            vert[3*i + 2][2] = 0.0;
+        // 2nd attribute buffer : colors
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(
+            1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+            3,                                // size
+            GL_FLOAT,                         // type
+            GL_FALSE,                         // normalized?
+            0,                                // stride
+            (void*)0                          // array buffer offset
+        );
+    }
+
+    void load_shaders() {
+        programID = LoadShaders( "TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader" );
+        
+        MatrixID = glGetUniformLocation(programID, "MVP");
+        PositionID = glGetUniformLocation(programID, "position");
+        printf("%d %d\n", MatrixID, PositionID);
+    }
+
+    void bindBuffers() {
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    }
+
+    public:
+        void init() {
+            glGenVertexArrays(1, &VertexArrayID);
+            glBindVertexArray(VertexArrayID);
+            load_shaders();
+            set_buffers();
         }
 
+        //glm::mat4 Projection, glm::mat4 View, glm::mat4 Model
+        void draw(float x, float y, float z, glm::mat4 &View) {
+            printf("draw called\n");
+		    glUseProgram(programID);
+            bindBuffers();
 
-      glBufferData(GL_ARRAY_BUFFER, vertex_count * 3 * 3 * sizeof(float), vert, GL_STATIC_DRAW );
-      glEnableVertexAttribArray(0);
-      glVertexAttribPointer(
-         0,                 // attribute 0, must match the layout in the shader.
-         3,       // size
-         GL_FLOAT,           // type
-         GL_FALSE,           // normalized?
-         0,//24,             // stride
-         (void*)0            // array buffer offset
-      );
-   }
-   void draw(float tx, float ty) {
-      bindProgram();
-      bindBuffers();
-      glUniform1f(0, 1.0/16);  // scale  in vertex shader
-      glUniform2f(1, tx, ty);  // center in vertex shader
-      glUniform3f(3, cross_color[0],cross_color[1],cross_color[2]);
+            glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+            // Camera matrix
 
-      glDrawArrays(GL_TRIANGLES, 0, __vertex_count * 3);
-   }
-   void setColor(float r, float g, float b){
-      cross_color[0]=r;cross_color[1]=g;cross_color[2]=b;
-   }
- private:
-    int __vertex_count = 0;
-   GLfloat cross_color[3] = { 0.0, 1.0, 0.0 };
+            glm::mat4 Model      = glm::mat4(1.0);
+            
+            glm::mat4 MVP = Projection * View * Model;
+    		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+            glUniform3f(PositionID, x, y, z);
+
+
+            glDrawArrays(GL_TRIANGLES, 0, 12*3);
+        }
 };
-
-#endif
--- lista_4/tutorial04.cpp
+++ lista_4/tutorial04.cpp
@@ -24,16 +24,64 @@ using namespace glm;
 
 #include "shape_renderers/cube.cpp"
 
-int main( void )
-{
-	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
-		return -1;
+float horizontalAngle = 3.14f;
+float verticalAngle = 0.0f;
+
+glm::vec3 CameraDirection;
+glm::vec3 CameraPosition;
+glm::mat4 ViewMatrix;
+
+void handle_controls() {
+	double mousex, mousey;
+	glfwGetCursorPos(window, &mousex, &mousey);
+	double mouseSpeed = 0.01;
+	// Reset mouse position for next frame
+	glfwSetCursorPos(window, 1024/2, 1024/2);
+	
+	horizontalAngle += mouseSpeed * float(1024/2 - mousex );
+	verticalAngle += mouseSpeed * float( 1024/2 - mousey );
+
+	CameraDirection = glm::vec3(
+		cos(verticalAngle) * sin(horizontalAngle), 
+		sin(verticalAngle),
+		cos(verticalAngle) * cos(horizontalAngle)
+	);
+
+	glm::vec3 right = glm::vec3(
+		sin(horizontalAngle - 3.14f/2.0f), 
+		0,
+		cos(horizontalAngle - 3.14f/2.0f)
+	);	
+
+	float deltaTime = 1.0;
+	float speed = 0.5;
+	// Move forward
+	if (glfwGetKey( window, GLFW_KEY_W ) == GLFW_PRESS){
+		CameraPosition += CameraDirection * deltaTime * speed;
+	}
+	// Move backward
+	if (glfwGetKey( window, GLFW_KEY_S ) == GLFW_PRESS){
+		CameraPosition -= CameraDirection * deltaTime * speed;
+	}
+	// Strafe right
+	if (glfwGetKey( window, GLFW_KEY_D ) == GLFW_PRESS){
+		CameraPosition += right * deltaTime * speed;
+	}
+	// Strafe left
+	if (glfwGetKey( window, GLFW_KEY_A ) == GLFW_PRESS){
+		CameraPosition -= right * deltaTime * speed;
 	}
 
+	glm::vec3 up = glm::cross(right, CameraDirection );
+	glm::vec3 eee = CameraPosition+CameraDirection;
+	ViewMatrix = glm::lookAt(CameraPosition, eee, up);	
+}
+
+
+int main( void )
+{
+
+	glfwInit();
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -41,7 +89,7 @@ int main( void )
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Tutorial 04 - Colored Cube", NULL, NULL);
+	window = glfwCreateWindow( 1024, 1024, "chuj", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
 		getchar();
@@ -50,199 +98,64 @@ int main( void )
 	}
 	glfwMakeContextCurrent(window);
 
-	// Initialize GLEW
-	
-	// MOD OR IS IT?
-	//glewExperimental = true; // Needed for core profile
-	/*
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-	*/
 
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
 	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	glClearColor(0.4f, 0.4f, 0.4f, 0.0f);
 
 	// Enable depth test
 	glEnable(GL_DEPTH_TEST);
 	// Accept fragment if it closer to the camera than the former one
 	glDepthFunc(GL_LESS); 
 
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
-	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader" );
-
-	// Get a handle for our "MVP" uniform
-	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-
 	// Projection matrix : 45� Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
 	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
 	// Camera matrix
-	glm::mat4 View       = glm::lookAt(
-								glm::vec3(5,3,-3), // Camera is at (4,3,-3), in World Space
-								glm::vec3(0,0,0), // and looks at the origin
-								glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-						   );
 	// Model matrix : an identity matrix (model will be at the origin)
 	glm::mat4 Model      = glm::mat4(1.0);
 	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+	glm::mat4 MVP        = Projection * ViewMatrix * Model; // Remember, matrix multiplication is the other way around
 
 	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-	static const GLfloat g_vertex_buffer_data[] = { 
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f, 1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f,
-		 1.0f, 1.0f, 1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f,
-		 1.0f, 1.0f, 1.0f,
-		 1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f
-	};
+	AGLErrors("uuuu");
 
-	// One color for each vertex. They were generated randomly.
-	static const GLfloat g_color_buffer_data[] = { 
-		0.583f,  0.771f,  0.014f,
-		0.609f,  0.115f,  0.436f,
-		0.327f,  0.483f,  0.844f,
-		0.822f,  0.569f,  0.201f,
-		0.435f,  0.602f,  0.223f,
-		0.310f,  0.747f,  0.185f,
-		0.597f,  0.770f,  0.761f,
-		0.559f,  0.436f,  0.730f,
-		0.359f,  0.583f,  0.152f,
-		0.483f,  0.596f,  0.789f,
-		0.559f,  0.861f,  0.639f,
-		0.195f,  0.548f,  0.859f,
-		0.014f,  0.184f,  0.576f,
-		0.771f,  0.328f,  0.970f,
-		0.406f,  0.615f,  0.116f,
-		0.676f,  0.977f,  0.133f,
-		0.971f,  0.572f,  0.833f,
-		0.140f,  0.616f,  0.489f,
-		0.997f,  0.513f,  0.064f,
-		0.945f,  0.719f,  0.592f,
-		0.543f,  0.021f,  0.978f,
-		0.279f,  0.317f,  0.505f,
-		0.167f,  0.620f,  0.077f,
-		0.347f,  0.857f,  0.137f,
-		0.055f,  0.953f,  0.042f,
-		0.714f,  0.505f,  0.345f,
-		0.783f,  0.290f,  0.734f,
-		0.722f,  0.645f,  0.174f,
-		0.302f,  0.455f,  0.848f,
-		0.225f,  0.587f,  0.040f,
-		0.517f,  0.713f,  0.338f,
-		0.053f,  0.959f,  0.120f,
-		0.393f,  0.621f,  0.362f,
-		0.673f,  0.211f,  0.457f,
-		0.820f,  0.883f,  0.371f,
-		0.982f,  0.099f,  0.879f
-	};
-
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-	GLuint colorbuffer;
-	glGenBuffers(1, &colorbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	Cube Ech;
+	Ech.init();
+	float counter = 0;
+	float size_mod = 1.0;
+	float px = 0.0;
+	float py = 15.0;
 
 	do{
+		handle_controls();
 
-		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Use our shader
-		glUseProgram(programID);
+
+	    AGLErrors("main-loopbegin");
 
 		// Send our transformation to the currently bound shader, 
 		// in the "MVP" uniform
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
-
-		// 2nd attribute buffer : colors
-		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-		glVertexAttribPointer(
-			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-			3,                                // size
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
-		);
 
 		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
 
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
+		Ech.draw(px, py, 0.0f, ViewMatrix);
+		//Ech.draw(0.0, 0.0, 0.0f, View);
+
+		//glDisableVertexAttribArray(0);
+		//glDisableVertexAttribArray(1);
 
 		// Swap buffers
+		AGLErrors("main-afterdraw");
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
-
-	// Cleanup VBO and shader
-	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteBuffers(1, &colorbuffer);
-	glDeleteProgram(programID);
-	glDeleteVertexArrays(1, &VertexArrayID);
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
